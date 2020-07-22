aac1567dc46c22c731cf4414a03e9a52a3d90699
ppcool1163 <prashantppmsd2405@gmail.com>
2020-07-23 01:02:24 +0530

    first commit

---
+++ public/script/index.js
@@ -0,0 +1,13 @@
+$('document').ready(function changeText() {
+  $('.lead-head').fadeIn(1000, function() {
+    $('.lead-head').fadeOut(1500,function(){
+      $('.lead-head').text('QUEST TO CONQUER')
+      $('.lead-head').fadeIn(1000,function(){
+        $('.lead-head').fadeOut(1500,function(){
+          $('.lead-head').text('#MADrush')
+          changeText()
+        })
+      })
+    })
+    })
+})
